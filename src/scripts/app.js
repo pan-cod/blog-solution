30390759664d01dee253939fa4bb3ccb2ff9e209
PANCOD Dariusz Markowicz <hello@pancod.com>
2019-01-10 13:27:15 +0100

    api request added

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,6 +3,30 @@
   Author: Dariusz Markowicz
  */
 
-import greet from './modules/greeting';
+import hyperHTML from 'hyperhtml';
+import axios from 'axios-es6';
 
-greet('World');
+const container = document.querySelector('.c-container');
+
+function generateArticles(data) {
+  container.append(hyperHTML.wire()`<article data-id=${data.id}>
+  <p>${data.author}</p>
+  <h1>${data.title}</h1>
+  <img src="${data.imageUrl}">
+  ${data.article.split('"')}
+  </article>`);
+}
+
+async function getArticles(url) {
+  let articles = {};
+  await axios.get(url).then(res => {
+    articles = res.data;
+  });
+  return articles;
+}
+
+const renderArticles = url => {
+  getArticles(url).then(item => item.map(a => generateArticles(a)));
+};
+
+renderArticles('https://stormy-shelf-93141.herokuapp.com/articles');
